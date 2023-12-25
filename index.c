7607bb28d0d70f5453d5f7e845c6da300ec5a1c2
satyajitsinh123 <satyajitsinh63@gamil.com>
2023-12-25 14:43:26 +0530

    hiii

---
+++ index.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("---A---\n");
+    printf("    *    \n");
+    printf("  *   *  \n");
+    printf(" * * * * \n");
+    printf("*       * \n");
+
+printf("---B---\n");
+printf("* * * *\n");
+printf("*      *\n");
+printf("* * * *\n");
+printf("*      *\n");
+printf("* * * *\n");
+
+printf("---c---\n");
+printf(" * * *\n");
+printf("* \n");
+printf("*\n");
+printf(" * * *\n");
+
+printf("---D---\n");
+printf("* * * *\n");
+printf("*       *\n");
+printf("*       *\n");
+printf("*       *\n");
+printf("* * * *\n");
+
+printf("---E---\n");
+printf("* * * *\n");
+printf("*      \n");
+printf("* * * *\n");
+printf("*      \n");
+printf("* * * *\n");
+
+printf("---F---\n");
+printf("* * * *\n");
+printf("*      \n");
+printf("* * * *\n");
+printf("*      \n");
+printf("*      \n");
+
+printf("---G---\n");
+printf(" * * *\n");
+printf("*    \n");
+printf("*    * * \n");
+printf("*      *\n");
+printf(" * * *\n");
+
+printf("---H---\n");
+printf("*     *\n");
+printf("*     *\n");
+printf("* * * *\n");
+printf("*     *\n");
+printf("*     *\n");
+
+printf("---I---\n");
+printf("* * * * *\n");
+printf("    *\n");
+printf("    *\n");
+printf("    *\n");
+printf("* * * * *\n");
+
+printf("---J---\n");
+printf("* * * * *\n");
+printf("    *\n");
+printf("    *\n");
+printf("    *\n");
+printf("* * \n");
+
+printf("---K---\n");
+printf("*     *\n");
+printf("*   *   \n");
+printf("* *  \n");
+printf("*   *  \n");
+printf("*     *\n");
+
+printf("---L---\n");
+printf("* \n");
+printf("*      \n");
+printf("* \n");
+printf("*      \n");
+printf("* * * *\n");
+
+printf("---M---\n");
+printf("*         *\n");
+printf("* *     * *\n");
+printf("*   *  *  *\n");
+printf("*    *    *\n");
+printf("*         *\n");
+
+
+printf("---N---\n");
+printf("*      *\n");
+printf("* *    * \n");
+printf("*   *  *  \n");
+printf("*    * *   \n");
+printf("*      *    \n");
+
+printf("---O---\n");
+printf("   * * * *\n");
+printf(" *         *\n");
+printf("*           *\n");
+printf("*           *\n");
+printf(" *         *\n");
+printf("   * * * *\n");
+
+printf("---P---\n");
+printf("* * * *\n");
+printf("*      *\n");
+printf("* * * *\n");
+printf("*      \n");
+printf("* \n");
+printf("* \n");
+
+printf("---Q---\n");
+printf("   * * * *\n");
+printf(" *         *\n");
+printf("*           *\n");
+printf("*       *   *\n");
+printf(" *       * *\n");
+printf("   * * * * *\n");
+printf("             *\n");
+
+printf("---R---\n");
+printf("* * * *\n");
+printf("*      *\n");
+printf("*     *\n");
+printf("* * * *  \n");
+printf("*    *\n");
+printf("*     *\n");
+printf("*      *\n");
+
+printf("---S---\n");
+printf(" * * * *\n");
+printf("*       \n");
+printf("* * * *\n");
+printf("       *\n");
+printf("* * * *\n");
+
+printf("---T---\n");
+printf("* * * * *\n");
+printf("    *\n");
+printf("    *\n");
+printf("    *\n");
+printf("    *\n");
+
+
+printf("---U---\n");
+printf("*            *\n");
+printf("*            * \n");
+printf("*            *\n");
+printf("*            *\n");
+printf(" *          *\n");
+printf("  *        *  \n");
+printf("   * * * *   \n");
+
+
+printf("---V---\n");
+printf("*           *\n");
+printf(" *         * \n");
+printf("  *       *\n");
+printf("   *     *\n");
+printf("    *   *\n");
+printf("     * *  \n");
+printf("      *    \n");
+
+printf("---W---\n");
+printf("*           *           *   \n");
+printf(" *         * *         *   \n");
+printf("  *       *   *       *   \n");
+printf("   *     *     *     *    \n");
+printf("    *   *       *   *    \n");
+printf("     * *         * *   \n");
+printf("      *           *  \n");
+
+
+
+
+printf("----X----\n") ;   
+    
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    printf(" * *\n");
+    printf("*   *\n");
+    
+    printf("---Y---\n") ;   
+    
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    printf("  *\n");
+    printf("  *\n");
+
+
+printf("---Z---\n") ;
+    printf("* * * *\n");
+    printf("    * \n");
+    printf("  * \n");
+    printf("* * * *\n");
+    
+
+
+    return 0;
+}
